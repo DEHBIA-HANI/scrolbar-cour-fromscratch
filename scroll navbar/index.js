5424f9b8db64b665ad33701e35aab1f3a27af1be
Dehbia Hani <129846731+DEHBIA-HANI@users.noreply.github.com>
2024-02-10 09:53:29 +0100

    Add files via upload

---
+++ scroll navbar/index.js
@@ -0,0 +1,11 @@
+// const navbar = document.getElementById("navbar");
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-60px";
+  }
+
+  lastScroll = window.scrollY;
+});
